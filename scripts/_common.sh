d245614ecb9264e3312da69550b512e3e5cf3291
Nathanael <nathanael@hannebert.com>
2021-04-12 07:59:21 +0200

    Add _common.sh file

---
+++ scripts/_common.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+
+# NodeJS version
+nodejs_version=14
+
+# MeshCentral Version
+meshcentral_version=0.7.89
+
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
